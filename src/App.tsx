e2f21c505ea61c9916a07951554af97ee79e850a
mrcastle <matthew.ray.castle@gmail.com>
2024-06-14 19:06:30 -0700

    Just some quick WIP stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Hero />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,7 +20,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+        <button
+          className="bg-stone-900 py-2 px-5 rounded-lg font-medium border border-transparent hover:border-indigo-400 focus:outline focus-visible:outline"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <div className="min-h-96 w-full pt-20  bg-slate-900">
+      <div className="max-w-96 mx-auto">
+        <div className="text-4xl">Matthew Castle</div>
+        <div className="text-xl">
+          I'm a software developer with a focus on clean code, ease of use, and
+          accessibility. I enjoy gaming, gardening and pretty rocks.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
