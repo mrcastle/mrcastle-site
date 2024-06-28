2bdd0c0e2f55729ddc664c20ded3d5d8ca743976
mrcastle <matthew.ray.castle@gmail.com>
2024-06-28 15:04:26 -0700

    Update app.tsx file to show orrery as a background

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+//import { useEffect, useState } from "react";
+
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 
-function App() {
-  const [count, setCount] = useState(0);
+import Orrery from "./components/Orrery/Orrery";
 
+function App() {
   return (
     <>
+      <Orrery />
       <Hero />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          className="button"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <button
-          className="bg-stone-900 py-2 px-5 rounded-lg font-medium border border-transparent hover:border-indigo-400 focus:outline focus-visible:outline"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
