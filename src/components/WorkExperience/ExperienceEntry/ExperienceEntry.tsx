dca5f8a35d083ecb322ba3f18a7c0eb4a0110eeb
mrcastle <matthew.ray.castle@gmail.com>
2024-07-03 14:23:08 -0700

    break out work experience to separate component and add skills display

---
+++ src/components/WorkExperience/ExperienceEntry/ExperienceEntry.tsx
@@ -0,0 +1,33 @@
+import { WorkExperience } from "../../../data/experience";
+
+type ExperienceEntryProps = {
+  experience: WorkExperience;
+};
+
+function ExperienceEntry({ experience }: ExperienceEntryProps) {
+  const { company, role, duration, description, skills } = experience;
+
+  return (
+    <>
+      <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-2.5 -start-1.5 border border-white dark:border-zinc-900 dark:bg-white-700"></div>
+      <div className="mb-4">
+        <h3 className="text-xl font-semibold text-emerald-300">{role}</h3>
+        <p className="text-gray-300">{company}</p>
+        <p className="text-gray-200 italic">{duration}</p>
+        <p className="text-white mt-2">{description}</p>
+        <div className="mt-4">
+          {skills.map((skill, index) => (
+            <span
+              key={`${role}-skill-${index}`}
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >
+              {skill}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ExperienceEntry;
--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -1,4 +1,5 @@
 import { getExperiences } from "../../data/experience";
+import ExperienceEntry from "./ExperienceEntry/ExperienceEntry";
 
 function WorkExperience() {
   const experiences = getExperiences();
@@ -9,16 +10,8 @@ function WorkExperience() {
         <h2 className="text-3xl font-bold mb-6">Experience</h2>
         <ol className="relative border-s border-gray-200 dark:border-white-700">
           {experiences.map((experience, index) => (
-            <li className="mb-10 ms-4" key={index}>
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-2.5 -start-1.5 border border-white dark:border-zinc-900 dark:bg-white-700"></div>
-              <div className="mb-4">
-                <h3 className="text-xl font-semibold text-emerald-300">
-                  {experience.role}
-                </h3>
-                <p className="text-gray-300">{experience.company}</p>
-                <p className="text-gray-200 italic">{experience.duration}</p>
-                <p className="text-white mt-2">{experience.description}</p>
-              </div>
+            <li className="mb-10 ms-4" key={`experience-${index}`}>
+              <ExperienceEntry experience={experience} />
             </li>
           ))}
         </ol>
