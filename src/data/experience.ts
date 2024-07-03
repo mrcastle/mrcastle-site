6f451e450f7f4b0535dba52bf9f54cfdc6d01d7b
mrcastle <matthew.ray.castle@gmail.com>
2024-07-03 14:21:21 -0700

    export types and add skills to experience data

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,8 +1,9 @@
-type WorkExperience = {
+export type WorkExperience = {
   role: string;
   company: string;
   duration: string;
   description: string;
+  skills: string[];
 };
 
 const experiences: WorkExperience[] = [
@@ -12,6 +13,14 @@ const experiences: WorkExperience[] = [
     duration: "March 2023 - January 2024",
     description:
       "Worked on developing and maintaining client communication and marketing insights services for dental practices.",
+    skills: [
+      "TypeScript",
+      "React",
+      "Node.js",
+      "PostgreSQL",
+      "Docker",
+      "Google Cloud Platform",
+    ],
   },
   {
     role: "Senior Frontend Developer",
@@ -19,6 +28,7 @@ const experiences: WorkExperience[] = [
     duration: "April 2021 - February 2023",
     description:
       "Led the frontend development creating mapping, visualization, and armory management apps for a firearms IoT device startup",
+    skills: ["TypeScript", "React", "Next.js", "CI/CD", "AWS", "Kubernetes"],
   },
   {
     role: "Software Developer",
@@ -26,6 +36,7 @@ const experiences: WorkExperience[] = [
     duration: "February 2018 - March 2021",
     description:
       "Developer for a primarily design-focused company working on various contracts; TiVo and the Small Business Administration of note.",
+    skills: ["React", "Typescript", "HTML", "CSS", "Ruby on Rails", "AWS"],
   },
   {
     role: "Software Developer",
@@ -33,6 +44,7 @@ const experiences: WorkExperience[] = [
     duration: "May 2017 - February 2018",
     description:
       "Assisted in the relaunch of an updated travel booking site and overhauled the Oracle Service Cloud integrations for a commercial travel company.",
+    skills: ["C#", ".NET", "Angular", "Azure", "Javascript"],
   },
   {
     role: "Software Developer",
@@ -40,6 +52,7 @@ const experiences: WorkExperience[] = [
     duration: "October 2011 - April 2017",
     description:
       "Led the development of an in-house suite of office productivity, timekeeping, and customer support apps.",
+    skills: ["C#", ".NET MVC", "Javascript", "HTML", "CSS", "jQuery"],
   },
 ];
 
--- src/data/user.ts
+++ src/data/user.ts
@@ -1,4 +1,4 @@
-type UserData = {
+export type UserData = {
   name: string;
   introduction: string;
   description: string;
