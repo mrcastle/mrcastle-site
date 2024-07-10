3ac5f2c13869c7995f9bbef0927b6952f35ff909
mrcastle <matthew.ray.castle@gmail.com>
2024-07-09 22:26:40 -0700

    Some color adjustments

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,13 +11,13 @@ function Hero() {
   return (
     <div className="mx-auto mt-40 mb-96 px-8 py-16 bg-zinc-900 shadow-lg  bg-clip-padding bg-opacity-40 border-zinc-900 backdrop-blur-sm">
       <div className="max-w-2xl mx-auto text-left">
-        <div className="text-xl text-emerald-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <div className="text-xl text-emerald-300 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           {introduction}
         </div>
         <div className="text-6xl  mt-1 mb-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           {name}
         </div>
-        <div className="text-xl text-emerald-200 mb-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <div className="text-xl text-emerald-300 mb-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           {description}
         </div>
         <div className="text-center">
--- src/components/WorkExperience/ExperienceEntry/ExperienceEntry.tsx
+++ src/components/WorkExperience/ExperienceEntry/ExperienceEntry.tsx
@@ -11,7 +11,7 @@ function ExperienceEntry({ experience }: ExperienceEntryProps) {
     <>
       <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-2.5 -start-1.5 border border-white dark:border-zinc-900 dark:bg-white-700"></div>
       <div className="mb-4">
-        <h3 className="text-xl font-semibold text-emerald-300">{role}</h3>
+        <h3 className="text-xl font-semibold text-emerald-400">{role}</h3>
         <p className="text-gray-300">{company}</p>
         <p className="text-gray-200 italic">{duration}</p>
         <p className="text-white mt-2">{description}</p>
